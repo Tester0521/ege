c5a4c9d5bdab3fccafea071ee15166f9964f6fdf
Dmitry <warfest.gg@mail.ru>
2023-02-02 21:12:00 +0300

    2 step

---
+++ 2.py
@@ -0,0 +1,16 @@
+def _2(vrai: bool=True) -> str:
+
+	print("x y z w")
+
+	for x in 0,1:
+		for y in 0,1:
+			for z in 0,1:
+				for w in 0,1:
+					F = ((not(x and not(y)) or (not(z) or not(w))) and ((not(w) or x) or y))
+					if vrai:
+						if F:
+							print(x, y, z, w)
+					else:
+						if not(F):
+							print(x, y, z, w)
+_2(False)
