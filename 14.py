840cea73cd9a263ffb785edcd6b183883a4e1d09
Dmitry <warfest.gg@mail.ru>
2023-02-04 17:11:31 +0300

    nothing

--- 14.py
+++ 14.py
@@ -1,10 +1,8 @@
 
 
-def to7() -> str:
-	x = 49**6 * 7**19 - 7**9 - 21
-	s, a = '', '0123456789'
-	while x > 0:
-		x, y = divmod(x, 7)
-		s += a[y]
-	return s.count('6')
-print(to7())
+def to7(x: int, b: int, al: str = '0123456789', fut = []) \
+		-> list: return fut.count(f'{b-1}') if x <= 0 else \
+		to7( x//b, b, fut = fut + [al[x%b]] )
+		
+print(to7(49**6 * 7**19 - 7**9 - 21, 7))
+
